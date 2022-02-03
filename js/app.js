658ba19e7f3e1978625b75ce200f201d65bbdcfc
Fabian Silva <silvafabian74@yahoo.com>
2022-02-03 16:54:27 -0500

    set variables and 1 cached element reference

--- js/app.js
+++ js/app.js
@@ -3,11 +3,16 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+//1.1
+let squares 
+//1.2
+let turn
+//1.3
+let isWinner = null
 
 /*------------------------ Cached Element References ------------------------*/
-
+const allSquares = document.querySelectorAll('.square')
+console.log(allSquares)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
