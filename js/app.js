11dd8587cb430b306f1242f30489efd430e31885
Fabian Silva <silvafabian74@yahoo.com>
2022-02-04 13:44:28 -0500

    add winning combos

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = {
+  [sq0, sq1, sq2],
+  [sq3, sq4, sq5],
+  [sq6, sq7, sq8],
+  [sq0, sq3, sq6],
+  [sq1, sq4, sq7],
+  [sq2, sq5, sq8],
+  [sq0, sq4, sq8],
+  [sq2, sq4, sq6]
+}
+console.log(winningCombos)
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 //1.1
-let squares 
-//1.2
-let turn
-//1.3
-let isWinner  
+let squares, turn, isWinner, playerX, playerO
+
 
 /*------------------------ Cached Element References ------------------------*/
 const allSquaresEl = document.querySelectorAll('.square')
@@ -18,7 +25,7 @@ const boardEl = document.querySelector('.board')
 const messageEl = document.getElementById('message')
 // console.log(messageEl)
 const resetButton = document.getElementById('reset-button')
-console.log(resetButton)
+// console.log(resetButton)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
