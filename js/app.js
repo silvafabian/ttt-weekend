f56ab19961e85124d1c23f0d9ef1f89776163334
Fabian Silva <silvafabian74@yahoo.com>
2022-02-04 13:17:25 -0500

    add event listener and board click function

--- js/app.js
+++ js/app.js
@@ -12,14 +12,30 @@ let isWinner
 
 /*------------------------ Cached Element References ------------------------*/
 const allSquaresEl = document.querySelectorAll('.square')
-// console.log(allSquares)
+// console.log(allSquaresEl)
 const boardEl = document.querySelector('.board')
+// console.log(boardEl)
 const messageEl = document.getElementById('message')
-console.log(messageEl)
+// console.log(messageEl)
+const resetButton = document.getElementById('reset-button')
+console.log(resetButton)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+allSquaresEl.forEach(square => {
+  square.addEventListener('click', boardClick)
+});
 
 
 /*-------------------------------- Functions --------------------------------*/
+init();
 
+function init(evt) {
+  squares =[sq0 = null, sq1 = null, sq2 = null, sq3 = null, sq4 = null, 
+    sq5 = null, sq6 = null, sq7 = null, sq8 = null];
+  // console.log(squares)
+}
+
+function boardClick (evt) {
+  console.log(evt.target.id)
+}
